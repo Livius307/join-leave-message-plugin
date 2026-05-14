78ff15c36353bb7fc54e8d0543de15b5531a1856
Livius307 <livius.wiebe@gmail.com>
2026-05-14 20:16:25 +0200

    added configuration

--- src/main/java/org/livius/joinLeaveMessage/JoinLeaveListener.java
+++ src/main/java/org/livius/joinLeaveMessage/JoinLeaveListener.java
@@ -8,20 +8,28 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class JoinLeaveListener implements Listener {
+    private JoinLeaveMessage plugin;
+
+    public JoinLeaveListener(JoinLeaveMessage plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
         Player player = e.getPlayer();
-        e.setQuitMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + player.getDisplayName() + ChatColor.DARK_RED + " has left. Bye!");
+        String lmessage = plugin.getConfig().getString("lmessage");
+        if (lmessage != null){
+            lmessage = lmessage.replace("%player%", e.getPlayer().getDisplayName());
+            e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', lmessage));
+        }
     }
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
-        if (player.hasPlayedBefore()) {
-            e.setJoinMessage(ChatColor.GREEN + "Welcome back to the server " + ChatColor.YELLOW + player.getDisplayName() + ChatColor.YELLOW + "!");
-        } else {
-            e.setJoinMessage(ChatColor.BLUE + "" + ChatColor.BOLD + "Welcome to the server " + ChatColor.BLUE + "" + player.getDisplayName());
+        String jmessage = plugin.getConfig().getString("jmessage");
+        if (jmessage != null){
+            jmessage = jmessage.replace("%player%", e.getPlayer().getDisplayName());
+            e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', jmessage));
         }
-
     }
 }
